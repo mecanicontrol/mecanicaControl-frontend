cfaf43d47aa24a252eaa7cf98007ddb88878260e
Claudio <cl.mohr@duocuc.cl>
2026-04-25 23:23:27 -0400

    feat: login conectado al backend

--- src/pages/Publico/Login.jsx
+++ src/pages/Publico/Login.jsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import InicioSesion from '../../components/login/InicioSesion'
+import { login } from '../../services/authService'
 
 export default function Login() {
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [resultado, setResultado] = useState(null)
+
+  const handleLogin = async (loginData) => {
+    try {
+      const { data } = await login({
+        email,
+        password,
+      })
+
+      setResultado(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
-        <Navbar />
-        <div>
-        <InicioSesion />
-        </div>
+      <Navbar />
+      
+      <div>
+        <InicioSesion 
+          email={email} 
+          password={password}
+          setEmail={setEmail}
+          setPassword={setPassword} 
+        />
+      </div>
 
-        <Footer />
+      <Footer />
     </div>
   )
 }
